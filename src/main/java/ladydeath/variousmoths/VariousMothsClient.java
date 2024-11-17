2593fe9fbfcd40d1fb22d989e3f6a25214f8bfa4
La-ryssa <mgamersilva9@gmail.com>
2024-11-17 10:25:45 -0300

    Initial commit

---
+++ src/main/java/ladydeath/variousmoths/VariousMothsClient.java
@@ -0,0 +1,10 @@
+package ladydeath.variousmoths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VariousMothsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
